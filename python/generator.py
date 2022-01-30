9009b0c3f1477902d9d22e25a98e2d60040fc4dd
Kiptoke <kiptoke@gmail.com>
2022-01-30 03:54:43 -0500

    Base code completed

---
+++ python/generator.py
@@ -0,0 +1,73 @@
+"""Builds a static HTML sites from a JSON file and a jinja2 HTML template."""
+
+import sys
+import pathlib
+import json 
+import jinja2
+import os
+import click
+
+# Main Project Code
+
+@click.command()
+@click.option('-v', '--verbose', is_flag=True, help='Prints more output.')
+def main(verbose):
+    """Static page generator for the Open Source Game Development Collection."""
+    
+    osgd = []
+    
+    path = os.path.abspath(os.path.join(os.getcwd(), 'repos'))
+    json_files = os.listdir(path)
+    
+    for file in json_files:
+        try:
+            json_file = open(f'{path}/{file}')
+            repo = json.load(json_file)
+            osgd.append(repo)
+            json_file.close()
+            if verbose:
+                print(f"-- File {file}.json loaded")
+        except FileNotFoundError:
+            print(f"FileNotFoundError: {file} not found")
+            sys.exit(1)
+        except ValueError:
+            print("JSON Error: Failed to decode json file")
+            sys.exit(1)
+
+    osgd_dict = {'osgd':osgd}
+
+    if verbose:
+        print(f"-- Writing to list_template.html ") 
+
+    # Open template file
+    try:
+        env = jinja2.Environment(
+            loader=jinja2.FileSystemLoader('templates'),
+            autoescape=jinja2.select_autoescape(['html', 'xml']),
+        )
+
+        template = env.get_template('list_template.html')  
+
+        if verbose:
+            print(f"-- list_template.html loaded")
+    except jinja2.TemplateNotFound:
+            print(f"Jinja Error: list_template.html does not exist")
+            sys.exit(1)
+
+    # Open output file
+    with open(f'public/list.html', 'w') as html:
+        if verbose:
+            print(f"-- Loaded list.html")
+    
+        # Fill out HTML template
+        try:
+            html.write(template.render(osgd_dict))
+        except jinja2.TemplateError:
+            print("Jinja Error: Error rendering template")
+            sys.exit(1)
+
+    if verbose:
+        print("-- Rendered list.html")
+    
+#if __name__ == "__main__":
+main()
--- python/repo.py
+++ python/repo.py
@@ -1,7 +1,25 @@
 import requests
 import json
+import sys
 from variables import auth
 
-with open('repos/aseprite.txt', 'w') as file:
-    r = requests.get('https://api.github.com/repos/aseprite/aseprite', auth=('kiptoke',auth.get('github')))
-    file.write(json.dumps(r.json(), indent=4))
+def main():
+    try:
+        json_file = open(f'list.json')
+        osgd = json.load(json_file)
+        json_file.close()
+    except ValueError:
+        print("JSON Error: Failed to decode json file")
+        sys.exit(1)
+
+    for repo in osgd["list"]:
+        repo_split = repo.split("/")
+        user = repo_split[0]
+        project = repo_split[1]
+    
+        with open(f'repos/{project.lower()}.json', 'w') as file:
+            r = requests.get(f'https://api.github.com/repos/{repo}', auth=('kiptoke',auth.get('github')))
+            file.write(json.dumps(r.json(), indent=4))
+  
+if __name__ == "__main__":
+    main()
