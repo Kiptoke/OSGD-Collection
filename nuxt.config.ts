0a18960651e4ef967a9d5d89938e19547bd22bc4
Andrew Zhou <kiptoke@gmail.com>
2024-01-22 23:08:32 -0500

    Set site title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    meta: {
+        title: 'Open Source Game Dev',
+    },
     modules: [
         '@inkline/plugin/nuxt',
         '@nuxtjs/supabase',
